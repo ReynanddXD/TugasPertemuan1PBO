7fd1d3039f33ea81abb54964b6cef734839ef846
ReynanddXD <165867557+ReynanddXD@users.noreply.github.com>
2024-08-27 19:19:43 +0700

    Add files via upload

---
+++ mahasiswa/Mahasiswa.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mahasiswa;
+
+/**
+ *
+ * @author USER
+ */
+public class Mahasiswa {
+    void membaca(){
+        System.out.println("Mahasiswa harus rajin membaca");
+    }
+    void nyontek(){
+        System.out.println("Mahasiswa jangan sering menyontek");
+    }
+    void modifikasi(){
+        System.out.println("Mahasiswa harus banyak modfikasi diri");
+    }  
+}
---
+++ mahasiswa/mahasiswaberaksi.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mahasiswa;
+
+public class mahasiswaberaksi {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa mahasiswa1 = new Mahasiswa();
+        
+        mahasiswa1.membaca();
+        mahasiswa1.nyontek();
+        mahasiswa1.modifikasi();
+    }
+}
